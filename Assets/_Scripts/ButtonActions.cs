42a2f01bbd787832dc7d7aea11d57bce0f05d8f8
Venky <venka.managoli@gmail.com>
2015-12-01 18:26:48 -0500

    Added screens

---
+++ Assets/_Scripts/ButtonActions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonActions : MonoBehaviour {
+
+	public void Start_Game () {
+		Application.LoadLevel ("Lvl1");
+	}
+	
+	public void Show_Instructions(){
+	}
+	
+	public void Quit_Game(){
+		Application.Quit ();
+	}	
+}
---
+++ Assets/_Scripts/thridlevel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class thridlevel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "Cube")
+		{
+			Application.LoadLevel("Lvl3");
+		}
+	}
+}
