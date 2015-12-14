71f45525ad363180da3416de71f406303bc4a0de
Venky <venka.managoli@gmail.com>
2015-12-14 14:33:18 -0500

    New scripts

--- Assets/_Scripts/ButtonActions.cs
+++ Assets/_Scripts/ButtonActions.cs
@@ -1,16 +1,74 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ButtonActions : MonoBehaviour {
-
-	public void Start_Game () {
-		Application.LoadLevel ("Lvl1");
+	public Canvas quitMenu;
+	public Canvas instructionMenu;
+	public Button startText;
+	public Button exitText;
+	public Button instructionText;
+	void Start ()
+		
+	{
+		quitMenu = quitMenu.GetComponent<Canvas>();
+		instructionMenu = instructionMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+		instructionText = instructionText.GetComponent<Button> ();
+		quitMenu.enabled = false;
+		instructionText.enabled = false;
+		instructionMenu.enabled = false;
+		
 	}
-	
-	public void Show_Instructions(){
+	public void ExitPress() //this function will be used on our Exit button
+		
+	{
+		quitMenu.enabled = true; //enable the Quit menu when we click the Exit button
+		startText.enabled = false; //then disable the Play and Exit buttons so they cannot be clicked
+		exitText.enabled = false;
+		instructionText.enabled = false;
+		instructionMenu.enabled = false;
+		
+	}
+	public void NoPress() //this function will be used for our "NO" button in our Quit Menu
+		
+	{
+		quitMenu.enabled = false; //we'll disable the quit menu, meaning it won't be visible anymore
+		startText.enabled = true; //enable the Play and Exit buttons again so they can be clicked
+		exitText.enabled = true;
+		instructionText.enabled = true;
+		instructionMenu.enabled = false;
+		
+	}
+	public void instPress()
+	{
+		quitMenu.enabled = false;
+		startText.enabled = false;
+		exitText.enabled = false;
+		instructionText.enabled = false;
+		instructionMenu.enabled = true;
+	}
+	public void Start_Game () {
+		Application.LoadLevel ("Level 1");
+	}
+
+
+	public void Instruction_game() {
+
+		instructionMenu.enabled = true;
 	}
 	
 	public void Quit_Game(){
 		Application.Quit ();
 	}	
 }
+
+
+
+
+
+
+
+
+
--- Assets/_Scripts/Lvl1ChestScript.cs
+++ Assets/_Scripts/Lvl1ChestScript.cs
@@ -1,12 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Lvl1ChestScript : MonoBehaviour {
-
+    public Text GemText;
+    int GemCount = 0;
+    bool guiEnable = false;
+    float timer = 0;
 	// Use this for initialization
 	void Start () {
-	
-	}
+        GemText.text = "Gems Collected " + GemCount + "/5";
+    }
 	
 	// Update is called once per frame
     void Update ()
@@ -14,8 +18,33 @@ public class Lvl1ChestScript : MonoBehaviour {
 
     }
 	void OnTriggerEnter (Collider col) {
-        Debug.Log("Hero");
-        if(col.CompareTag("Hero")){Application.LoadLevel("Lvl2");
+        Debug.Log(col.name);
+        if (col.CompareTag("Hero"))
+        {
+            Debug.Log("Hero");
+            if (GemCount == 5)
+                    Application.LoadLevel("Level 2");
+            else
+            {
+                guiEnable = true;
+                OnGUI();
+            }
         }
-	}
+    }
+
+    public void OnGUI()
+    {
+        while (guiEnable == true) {
+            GUI.Label(new Rect(250, Screen.height / 2 - 25, Screen.width, 50), "message");
+            timer += Time.deltaTime;
+            if (timer == 5)
+            {
+                guiEnable = false;
+                timer = 0;
+            }
+ 
+        }
+        
+    }
 }
+
