c6dda3ad814e28392ad6e44193720732722ee472
vips95 <arora.vips@gmail.com>
2015-12-18 12:50:11 -0500

    changes

---
+++ Assets/_Scripts/Lvl1Message.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lvl1Message : MonoBehaviour {
+
+	float timer = 0;
+	bool guienable = true;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (guienable == true) {
+			OnGUI ();
+		}
+	}
+
+	public void OnGUI(){
+		GUI.Label (new Rect (400, 650, 300, 100), "Collect at least 5 gems before proceeding to next level");
+		timer += Time.deltaTime;
+		Debug.Log(timer);
+		if (timer > 10.0f) {
+			guienable = false;
+			timer = 0;
+		}
+	}
+}
