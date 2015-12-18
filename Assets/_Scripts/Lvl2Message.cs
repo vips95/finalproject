85c1f8563ba0829af80555889ecb3790eff593ae
vips95 <arora.vips@gmail.com>
2015-12-18 18:56:50 -0500

    added instructions in every scene

--- Assets/_Scripts/Lvl2Message.cs
+++ Assets/_Scripts/Lvl2Message.cs
@@ -1,8 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Lvl1Message : MonoBehaviour {
-
+public class Lvl2Message : MonoBehaviour {
 	float timer = 0;
 	bool guienable = true;
 	GUIStyle largefont;
@@ -10,24 +9,24 @@ public class Lvl1Message : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		largefont = new GUIStyle ();
-		 largefont.fontSize = 40;
+		largefont.fontSize = 40;
 		smallfont = new GUIStyle ();
-		 smallfont.fontSize = 30;
+		smallfont.fontSize = 30;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-			OnGUI ();
-
+		OnGUI ();
+		
 	}
-
+	
 	public void OnGUI(){
 		GUI.contentColor = Color.black;
-
+		
 		if(guienable==true){
-			GUI.Label (new Rect (100, 440, 300, 100), "Level 1",largefont); 
-			GUI.Label (new Rect (10,500,300,100),"Collect at least 5 gems \nbefore proceeding to next level",smallfont);
+			GUI.Label (new Rect (100, 440, 300, 100), "Level 2",largefont); 
+			GUI.Label (new Rect (10,500,300,100),"Either kill or dodge the enemies to reach \nthe door which leads to the next level",smallfont);
 			timer += Time.deltaTime;
 			if (timer > 10.0f) {
 				guienable = false;
---
+++ Assets/_Scripts/gameOverScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class gameOverScript : MonoBehaviour {
+    public Canvas quitmenu;
+    public Button Restart;
+    public Button quit;
+    public Text score;
+    public Text gamecomplete;
+	// Use this for initialization
+	void Start () {
+        quitmenu = quitmenu.GetComponent<Canvas>();
+        Restart = Restart.GetComponent<Button>();
+        quit = quit.GetComponent<Button>();
+        score = score.GetComponent<Text>();
+        gamecomplete = gamecomplete.GetComponent<Text>();
+        quit.enabled = true;
+        Restart.enabled = true;
+        score.enabled = true;
+        gamecomplete.enabled = true;
+		quitmenu.enabled = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void restart_game()
+    {
+		Application.LoadLevel ("Level 1");
+    }
+   public void quit_game()
+    {
+        quitmenu.enabled = true;
+        Restart.enabled = false;
+        quit.enabled = false;
+        score.enabled = false;
+        gamecomplete.enabled = false;
+    }
+   public void nopress()
+    {
+        Restart.enabled = true;
+        quit.enabled = true;
+        score.enabled = true;
+            gamecomplete.enabled = true;
+            quitmenu.enabled = false;
+    }
+   public void yespress()
+    {
+        Application.Quit();
+    }
+}
